b535a4fbe49e91c5240b6fe945fdd9586ee41edd
sbarnwell88 <susy6688@gmail.com>
2017-07-12 12:25:04 -0400

    created alphabet and displayed them on page

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,20 @@
+//create a container with the alphabet
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+//display the alphabet on the page
+$('#buttons').append(alphabet).html;
+//display the alphabet in boxes
 
+//GIVEN the page loads
+//WHEN I clicks on a letter
+//THEN I see the letters grey out
 
+//GIVEN the page loads
+//WHEN I click on the instruction buttons
+//THEN a widow pops up where I can read the instructions of the game
 
+//GIVEN the page is loaded
+//WHEN I look at the page
+//THEN the lines appear that equal the length of the word that's being guessed
 
 
 $(function(){
