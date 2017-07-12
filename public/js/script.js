e541b02bb1de292f007d5d20cf15706cff9f1ad9
sbarnwell88 <susy6688@gmail.com>
2017-07-12 14:28:17 -0400

    added bootstrap and put letters into buttons

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,40 @@
 //create a container with the alphabet
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 //display the alphabet on the page
-$('#buttons').append(alphabet).html;
-//display the alphabet in boxes
+for (var i = 0; i < alphabet.length; i++) {
+    $('#buttons-for-letters').append('<button>'+alphabet[i]+'</button>');
+};
+//create button for each letter
 
-//GIVEN the page loads
+
+
 //WHEN I clicks on a letter
 //THEN I see the letters grey out
 
-//GIVEN the page loads
 //WHEN I click on the instruction buttons
 //THEN a widow pops up where I can read the instructions of the game
 
-//GIVEN the page is loaded
-//WHEN I look at the page
-//THEN the lines appear that equal the length of the word that's being guessed
+//create lines that match the length of the word
+//create word bank
 
+
+//WHEN I click on a correct letter
+//THEN I see the letter appear on the line
+
+//WHEN I click on the incorrect letter
+//THEN I see the "number of lives" go down
+
+//WHEN I guess a letter
+//THEN I see the number of attempts I have left
+
+//WHEN I guess the last correct letter
+//THEN I see "You Win!"
+
+//WHEN I make my last attempt
+//THEN display "You lose. Try again"
+
+//WHEN I'm finished playing a game
+//THEN I can click on "play again"
 
 $(function(){
     
